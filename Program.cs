053da9e453d174601c5661b3cd38cec1b9d89543
nicolas.carvalho <nicolas.carvalho@cgm.fortaleza.ce.gov.br>
2024-03-11 16:34:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+System.Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("12332112", "Fone", "11111111111", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+System.Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone("312123123", "iPhone 12", "2222222222", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Wild Rift");
